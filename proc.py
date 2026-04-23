0de2be1f0f52e0eaa4abd54d2f0ba2225a99d874
Prajeet <prajeet@Akaikes-MacBook-Air.local>
2026-04-23 15:00:36 +0530

    init proc commit

--- proc.py
+++ proc.py
@@ -1,14 +1,16 @@
-
 from yt_harvest_dwnld import search_download, channel_download, FilterConfig
 
 
 channel_download(
     channel_url="https://www.youtube.com/@AmazonMXPlayer",
     output_dir="./audio/channel",
-    min_duration=600,       # 10 min
-    max_duration=3600,      # "full" | "shorts" | "both"
+    min_duration=1200,  
+    max_results=50, 
+    max_downloads=20,
     audio_format="wav",
     transcript_lang="hi",
-    rate_limit="10M",
+    rate_limit="2000",
+    filters=FilterConfig(
+          title_must_contain=["Episode"],                                                                                                                
+      ),                                                                                                                                                                                               
 )
-
